37fdb7fd42998b453f3a275dc9e3ff9f3f53ca9c
anigno <anigno@gmail.com>
2024-12-13 21:27:45 +0200

    added disconnection handler registration

--- tcp_channel.py
+++ tcp_channel.py
@@ -2,6 +2,7 @@ import logging
 import socket
 import threading
 import time
+from typing import Optional
 
 from logging_lib.simple_logging.simple_logger import SimpleLogger
 
@@ -18,9 +19,10 @@ class TcpChannel:
         self.is_listening = None
         self.is_data_receiving = None
         self.listening_socket = None
-        self.client_socket: socket.socket = None
+        self.client_socket: Optional[socket].socket = None
         self.client_address = None
         self.on_data_received_handler = None
+        self.on_connection_error = None
         self.server_client_receiving_socket = None
         self.client_sending_socket = None
         self.channel_name = f"[{self.__class__.__name__} {self.listening_host}:{self.listening_port}]"
@@ -39,6 +41,9 @@ class TcpChannel:
 
     def register_data_received_callback(self, on_data_received_handler: callable):
         self.on_data_received_handler = on_data_received_handler
+
+    def register_connection_error_callback(self, on_connection_error: callable):
+        self.on_connection_error = on_connection_error
 
     def close(self):
         """close connection listener ,server_client_receiving_socket and client_sending_socket"""
@@ -63,6 +68,8 @@ class TcpChannel:
             self.client_socket.sendall(data_length_bytes + data)
         except Exception as ex:
             self.logger.error(f"{self.channel_name} {ex}")
+            if self.on_connection_error:
+                self.on_connection_error(ex)
 
     def listening_thread_handler(self):
         self.listening_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -98,16 +105,23 @@ class TcpChannel:
                 data = b"".join(received_buffers)
                 if data:
                     self.logger.debug(f"{self.channel_name} received data: {len(data)} bytes")
-                    self.on_data_received_handler(data)
+                    if self.on_data_received_handler:
+                        self.on_data_received_handler(data)
             except OSError as ex:
                 if ex.winerror == 10038:
                     self.logger.info(f"{self.channel_name} socket closed,stopped receiving [WinError 10038]")
                     return
                 if self.is_data_receiving:
-                    self.logger.warning(f"{self.channel_name} {ex}")
+                    self.logger.error(f"{self.channel_name} {ex}")
+                    if self.on_connection_error:
+                        self.on_connection_error(ex)
+                    return
             except Exception as ex:
                 if self.is_data_receiving:
                     self.logger.error(f"{self.channel_name} {ex}")
+                    if self.on_connection_error:
+                        self.on_connection_error(ex)
+                    return
 
 if __name__ == '__main__':
     SimpleLogger("my_logger", logging.DEBUG)
@@ -132,6 +146,7 @@ if __name__ == '__main__':
         ch1.send(b"12345678901" * 100 * 1024 * 1024)
         ch1.send(b"123456")
         ch2.send(b"abcdefgh")
+
         while len(results) < 6:
             time.sleep(.5)
         ch1.close()
@@ -155,6 +170,26 @@ if __name__ == '__main__':
             my_logger.error(f"expected error: {ex}")
         ch1.close()
 
+    def connect_disconnect():
+        def on_connection_error(ex: Exception):
+            my_logger.info(f"****** {ex}")
+
+        ch1 = TcpChannel(my_logger, "localhost", 9020)
+        ch1.register_connection_error_callback(on_connection_error)
+        ch1.start_listening()
+        ch2 = TcpChannel(my_logger, "localhost", 9021)
+        ch2.register_connection_error_callback(on_connection_error)
+        ch2.start_listening()
+        ch1.connect("localhost", 9021)
+        ch1.send(b"123456")
+        ch2.send(b"abcdefgh")
+        time.sleep(0.5)
+        # closing ch2 and sending
+        ch2.close()
+        ch1.send(b"12345678")
+        # closing all
+        ch1.close()
+
     connect_send_receive()
     connection_error()
-
+    connect_disconnect()
